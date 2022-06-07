edb5001405d5447ca6b7856af4528f8bc66cc0b7
Alexander Alexandrov <aalexandrov4@dxc.com>
2022-06-07 17:40:16 +0300

    Add styles

--- svelte-project/src/App.svelte
+++ svelte-project/src/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     export let wasm;
 
-    import {ProgressCircular, MaterialApp, TextField, Button, Col} from 'svelte-materialify';
+    import {ProgressCircular, MaterialApp, TextField, Button, Col, Row} from 'svelte-materialify';
     import Persons from "./Persons.svelte";
 
     wasm.get_state().names.map(name => console.log("ID: ", name.id, "; FIRST_NAME: ", name.first_name));
@@ -44,29 +44,41 @@
 </script>
 
 <MaterialApp>
-    <img src="images/rust_logo.png" alt="background image" width="60"/>
-    <img src="images/arrow_right_icon.png" alt="background image" width="60"/>
-    <img src="images/wasm_logo.png" alt="background image" width="60"/>
-    <img src="images/plus_icon.png" alt="background image" width="60"/>
-    <img src="images/svelte_logo.png" alt="background image" width="60"/>
-    <br/>
-    <br/>
-    <Col cols={2}>
-        {#if progress}
-            <ProgressCircular indeterminate color="red" size={70} width={7}/>
-        {/if}
-        <br/>
-        <br/>
-        <TextField bind:value={name} clearable color="red">
-            Name
-        </TextField>
-        <Button class="red white-text" on:click={() => handleAddName()}>
-            Add name
-        </Button>
-        <br/>
-        <br/>
-        <Persons bind:state/>
-    </Col>
+    <style>
+        .container {
+            padding: 8px;
+            margin-bottom: 12px;
+            background-color: var(--theme-app-bar);
+        }
+    </style>
+
+    <div class="container">
+        <Row class="align" style="height:600px">
+            <Col cols={2} offset={6} offset_md={4}>
+                <img src="images/rust_logo.png" alt="background image" width="55"/>
+                <img src="images/arrow_right_icon.png" alt="background image" width="55"/>
+                <img src="images/wasm_logo.png" alt="background image" width="55"/>
+                <img src="images/plus_icon.png" alt="background image" width="55"/>
+                <img src="images/svelte_logo.png" alt="background image" width="55"/>
+                <br/>
+                <br/>
+                {#if progress}
+                    <ProgressCircular indeterminate color="red" size={70} width={7}/>
+                {/if}
+                <br/>
+                <br/>
+                <TextField bind:value={name} clearable color="red">
+                    Name
+                </TextField>
+                <Button class="red white-text" on:click={() => handleAddName()}>
+                    Add name
+                </Button>
+                <br/>
+                <br/>
+                <Persons bind:state/>
+            </Col>
+        </Row>
+    </div>
 </MaterialApp>
 
 
